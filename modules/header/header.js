af83fe9bfdbdc8f58fbb6fa960c8533be7aa52df
Mario Cagalj <mario.cagalj@fesb.hr>
2021-10-20 15:22:09 +0200

    Add bg image and fix hdr elements positions

--- modules/header/header.js
+++ modules/header/header.js
@@ -3,26 +3,40 @@ import styles from './header.module.css';
 import Image from 'next/image';
 
 import LogoImg from '../../assets/logo.png';
+import HeroImg from '../../assets/hero.png';
 
 const Header = () => {
     return (
         <header className={styles.main}>
-            <main className={styles.content}>
+            <div className={styles.bgWrap}>
                 <Image
-                    src={LogoImg}
-                    layout="fixed"
-                    width={50}
-                    height={50}
-                    alt="Design logo"
+                    src={HeroImg}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Hero image"
                 />
-                <nav className={styles.landingText}>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Showcase</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                    <li>Sign In</li>
-                </nav>
+            </div>
+            <main className={styles.content}>
+                <div className={styles.menuWrap}>
+                    <Image
+                        src={LogoImg}
+                        layout="fixed"
+                        width={50}
+                        height={50}
+                        alt="Design logo"
+                    />
+
+                    <nav>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Showcase</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                        <li>Sign In</li>
+                    </nav>
+                </div>
+                <h1>Design Matters</h1>
+                <h3>Entrust us with your digital appearance</h3>
             </main>
         </header>
     );
