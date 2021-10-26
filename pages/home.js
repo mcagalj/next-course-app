fa1e99a0e3ee9f392a25582f7745fcced7212b37
Mario Cagalj <mario.cagalj@fesb.hr>
2021-10-26 16:47:38 +0200

    Add and style CTA section

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,59 @@
-import React from 'react';
 import Header from '../modules/header/header.js';
+import Image from 'next/image';
 
-const Hello = () => <Header />;
+import CTAImg from '../assets/grow_business.jpg';
+
+// TBD: Refactor this
+const width = 410;
+const height = width / (508 / 440);
+
+const Hello = () => {
+    return (
+        <>
+            <Header />
+            {/* Call to action (CTA) section */}
+            <section className="pt-10">
+                <main className="max-w-4xl mx-auto flex align-bottom">
+                    <div className="flex">
+                        <Image
+                            src={CTAImg}
+                            layout="fixed"
+                            width={width}
+                            height={height}
+                            alt="Grow business"
+                        />
+                    </div>
+                    <div className="ml-8 flex flex-col justify-between">
+                        <div>
+                            <div>
+                                <h2 className="capitalize text-4xl font-roboto-condensed font-bold text-gray-700">
+                                    Grow your business with us
+                                </h2>
+                                <h4 className="text-xl text-gray-400 mt-2">
+                                    Beautify your website and brand
+                                </h4>
+                            </div>
+                            <p className="mt-4 leading-6">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore magna aliqua. Ut enim ad minim veniam, quis
+                                nostrud exercitation ullamco laboris nisi ut aliquip
+                                ex ea commodo consequat.
+                            </p>
+                            <p className="mt-2 leading-6">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore magna aliqua.
+                            </p>
+                        </div>
+                        <button className="w-2/3 py-3 bg-red-700 hover:bg-red-800 shadow-md whitespace-nowrap text-xl text-white font-medium">
+                            Book a meeting
+                        </button>
+                    </div>
+                </main>
+            </section>
+        </>
+    );
+};
 
 export default Hello;
