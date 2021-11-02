54904af76464924671fc956f87dc5a8900df6fd0
Mario Cagalj <mario.cagalj@fesb.hr>
2021-11-02 17:10:15 +0100

    Add a mobile version of the testimonial section

--- pages/home.js
+++ pages/home.js
@@ -37,8 +37,8 @@ const Hello = () => {
 
                     <div className="mt-14 mb-4 px-10 sm:hidden">
                         <h2 className="capitalize text-3xl font-roboto-condensed font-bold text-gray-700">
-                            Grow your business <br />
-                            <span className="text-hci-lila">with us</span>
+                            Grow your business{' '}
+                            <span className="capitalize text-hci-lila">with us</span>
                         </h2>
                     </div>
 
@@ -120,27 +120,42 @@ const Hello = () => {
             </section>
 
             {/* Testimonials section */}
-            <section className="mt-12 py-12 bg-gray-50 hidden sm:block">
+            <section className="mt-12 sm:py-12 sm:bg-gray-50">
                 <main className="max-w-4xl flex flex-col mx-auto">
-                    <div>
-                        <h2 className="capitalize text-4xl font-roboto-condensed font-bold text-gray-700">
+                    <div className="px-10">
+                        <h2 className="capitalize text-3xl sm:text-4xl font-roboto-condensed font-bold text-gray-700">
                             What our customers are saying
                         </h2>
                         <h4 className="text-xl text-gray-400 mt-2">
                             Read case studies of our happy customers
                         </h4>
                     </div>
-                    <div className="mt-12 grid grid-cols-2 gap-4">
-                        <div className="h-96 border hover:cursor-pointer">
+                    <div className="mt-12 sm:grid sm:grid-cols-2 sm:gap-4">
+                        <div className="h-96 sm:border hover:cursor-pointer first:mt-0">
                             <div className="h-4/5 bg-gray-100 relative">
                                 <Image
                                     src={'/design-system.jpg'}
                                     layout="fill"
                                     objectFit="cover"
-                                    alt="Grow business"
+                                    objectPosition="0px 0px"
+                                    alt="Design system"
                                 />
                             </div>
-                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative">
+                            <div className="flex px-10 items-center relative mt-8 sm:hidden">
+                                <p className="uppercase text-xl font-semibold text-hci-lila">
+                                    New design system
+                                </p>
+                                <div className="absolute right-10 h-full flex items-center">
+                                    <Image
+                                        src={'/right.svg'}
+                                        layout="fixed"
+                                        width={15}
+                                        height={15}
+                                        alt="Right icon"
+                                    />
+                                </div>
+                            </div>
+                            <div className="h-1/5 bg-gray-300 items-center justify-center relative hidden sm:flex">
                                 <p className="capitalize font-roboto-condensed text-xl text-hci-lila">
                                     New design system
                                 </p>
@@ -155,16 +170,30 @@ const Hello = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="h-96 border hover:cursor-pointer">
+                        <div className="h-96 sm:border hover:cursor-pointer my-12 sm:mt-0">
                             <div className="h-4/5 bg-gray-100 relative">
                                 <Image
                                     src={'/from-scratch.jpg'}
                                     layout="fill"
                                     objectFit="cover"
-                                    alt="Grow business"
+                                    alt="Design from scratch"
                                 />
                             </div>
-                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative">
+                            <div className="flex px-10 items-center relative mt-8 sm:hidden">
+                                <p className="uppercase text-xl font-semibold text-hci-lila">
+                                    Design from scratch
+                                </p>
+                                <div className="absolute right-10 h-full flex items-center">
+                                    <Image
+                                        src={'/right.svg'}
+                                        layout="fixed"
+                                        width={15}
+                                        height={15}
+                                        alt="Right icon"
+                                    />
+                                </div>
+                            </div>
+                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative hidden sm:flex">
                                 <p className="capitalize font-roboto-condensed text-xl text-hci-lila">
                                     Design from scratch
                                 </p>
@@ -179,16 +208,31 @@ const Hello = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="h-96 border hover:cursor-pointer">
+                        <div className="h-96 sm:border hover:cursor-pointer my-12 sm:mt-0">
                             <div className="h-4/5 bg-gray-100 relative">
                                 <Image
                                     src={'/brand.png'}
                                     layout="fill"
                                     objectFit="cover"
-                                    alt="Grow business"
+                                    objectPosition="0px 0px"
+                                    alt="Band transformation"
                                 />
                             </div>
-                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative">
+                            <div className="flex px-10 items-center relative mt-8 sm:hidden">
+                                <p className="uppercase text-xl font-semibold text-hci-lila">
+                                    Brand transformation
+                                </p>
+                                <div className="absolute right-10 h-full flex items-center">
+                                    <Image
+                                        src={'/right.svg'}
+                                        layout="fixed"
+                                        width={15}
+                                        height={15}
+                                        alt="Right icon"
+                                    />
+                                </div>
+                            </div>
+                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative hidden sm:flex">
                                 <p className="capitalize font-roboto-condensed text-xl text-hci-lila">
                                     Brand transformation
                                 </p>
@@ -203,15 +247,29 @@ const Hello = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="h-96 border hover:cursor-pointer">
+                        <div className="h-96 sm:border hover:cursor-pointer">
                             <div className="h-4/5 bg-gray-100 relative">
                                 <Image
                                     src={'/book-cover.jpg'}
                                     layout="fill"
-                                    alt="Grow business"
+                                    alt="Book cover design"
                                 />
                             </div>
-                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative">
+                            <div className="flex px-10 items-center relative mt-8 sm:hidden">
+                                <p className="uppercase text-xl font-semibold text-hci-lila">
+                                    Book cover design
+                                </p>
+                                <div className="absolute right-10 h-full flex items-center">
+                                    <Image
+                                        src={'/right.svg'}
+                                        layout="fixed"
+                                        width={15}
+                                        height={15}
+                                        alt="Right icon"
+                                    />
+                                </div>
+                            </div>
+                            <div className="h-1/5 bg-gray-300 flex items-center justify-center relative hidden sm:flex">
                                 <p className="capitalize font-roboto-condensed text-xl text-hci-lila">
                                     Book cover design
                                 </p>
@@ -227,7 +285,7 @@ const Hello = () => {
                             </div>
                         </div>
                     </div>
-                    <button className="capitalize mt-12 mb-12 mx-auto w-1/3 py-3 border shadow-md whitespace-nowrap text-xl text-hci-lila font-medium hover:bg-gray-100">
+                    <button className="capitalize mt-12 mb-12 mx-auto px-10 py-3 border shadow-md whitespace-nowrap text-xl text-hci-lila font-medium hover:bg-gray-100">
                         View showcase
                     </button>
                 </main>
