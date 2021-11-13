86997781668e4faba297a9667e232fe8d61aa4bb
Mario Cagalj <mario.cagalj@fesb.hr>
2021-11-13 14:27:14 +0100

    Quick Cloudinary test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     reactStrictMode: true,
     images: {
+        // loader: 'cloudinary',
         domains: ['dl.airtable.com'],
+        // domains: ['res.cloudinary.com'],
     },
 };
