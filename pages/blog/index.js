b00e5a0122e986740a4e6509f92aa5cf9675d170
Mario Cagalj <mario.cagalj@fesb.hr>
2021-11-16 15:03:24 +0100

    Add blog folder

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ const Blog = () => {
         <section className="sm:py-12 sm:bg-gray-50">
             <main className="max-w-4xl flex flex-col mx-auto my-8">
                 <h2 className="capitalize text-3xl sm:text-4xl font-roboto-condensed font-bold text-gray-700 px-10 sm:px-0">
-                    Blog
+                    Blog Index Page
                 </h2>
             </main>
         </section>
